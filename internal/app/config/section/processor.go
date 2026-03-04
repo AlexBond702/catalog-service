d02f34e68a68a5f7193075da41eb4773081a625b
AlexBond702 <bond.alex.702@gmail.com>
2026-03-04 16:52:48 +0500

    TASK-1001:ВЫÑВынес Ñ.env из Ñ.gitignore,Ð дописал теги для WebServer

--- internal/app/config/section/processor.go
+++ internal/app/config/section/processor.go
@@ -2,7 +2,7 @@ package section
 
 type (
 	Processor struct {
-		WebServer ProcessorWebServer
+		WebServer ProcessorWebServer `required:"true" split_words:"true"`
 	}
 	ProcessorWebServer struct {
 		ListenPort uint32 `required:"true" default:"8080" split_words:"true"`
