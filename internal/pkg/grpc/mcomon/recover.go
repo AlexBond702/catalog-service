f548bbfedcda1416b04f403e0cdb884ebe3dc5c1
AlexBond702 <bond.alex.702@gmail.com>
2026-06-25 11:44:00 +0500

    TASK-4002:Настроил метрики и борд для gRPC-Server(catalog)

---
+++ internal/pkg/grpc/mcomon/recover.go
@@ -0,0 +1,56 @@
+package mcomon
+
+import (
+	"context"
+	"fmt"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/AlexBond702/catalog-service/internal/pkg/grpc/grpch"
+)
+
+type recoveryMiddleware struct{}
+
+func NewRecoveryMiddleware() grpch.Middleware {
+	return new(recoveryMiddleware)
+}
+
+func (r *recoveryMiddleware) ForUnary() grpc.UnaryServerInterceptor {
+	return func(
+		ctx context.Context,
+		req any,
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (resp any, err error) {
+		panicked := true
+		defer func() {
+			if rec := recover(); rec != nil || panicked {
+				err = status.Errorf(codes.Internal, "%s", fmt.Sprintf("recovery panic: %v - %t", rec, panicked))
+			}
+		}()
+		resp, err = handler(ctx, req)
+		panicked = false
+		return resp, err
+	}
+}
+
+func (r *recoveryMiddleware) ForStream() grpc.StreamServerInterceptor {
+	return func(
+		srv any,
+		cc grpc.ServerStream,
+		info *grpc.StreamServerInfo,
+		handler grpc.StreamHandler,
+	) (err error) {
+		panicked := true
+		defer func() {
+			if rec := recover(); rec != nil {
+				err = status.Errorf(codes.Internal, "%s", fmt.Sprintf("recovery panic: %v - %t", rec, panicked))
+			}
+		}()
+		err = handler(srv, cc)
+		panicked = false
+		return err
+	}
+}
